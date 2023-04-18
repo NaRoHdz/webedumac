5acb5f27412533b02b366f6a6a8444a0476140e0
NaRoHdz <normange_rg@hotmail.com>
2023-04-18 11:41:03 -0600

    normaWeb23

---
+++ prueba.js
@@ -0,0 +1,27 @@
+console.log("hola");
+
+var string = "NormaRosas";
+var number = 35;
+var boolean = true;
+
+console.log (string);
+console.log (number);
+console.log (boolean);
+
+
+let x = 5;
+console.log (x);
+x=10;
+
+console.log(x);
+
+
+var x= 10;
+var y= 20;
+var suma= x+y;
+console.log (suma);
+
+console.log (typeof(suma));
+console.log (suma);
+console.log (typeof (String (suma));
+console.log (String(suma));
